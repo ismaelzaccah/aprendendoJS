60dc9c20ad2bfa275f9124d4f98ffb15427c8efa
Ismael Zaccah <ismael.zaccah@gmail.com>
2021-07-15 10:15:00 -0300

    iniciando os estudos em Javascript

---
+++ app.js
@@ -0,0 +1,17 @@
+var name = 'Ismael Zaccah' // string
+var age = 25 //integer
+var weight = 70.5 // float
+var alive = true // boolean
+var food = null // null
+var time //undefined
+
+var fruits = ['Morango','Maçã', 'Abacate', 'Pera'] // array
+
+var person = {
+    name: 'ismael',
+    age: 25,
+    alive: true,
+    height: 172.5,
+} // object
+
+console.log(typeof person)
