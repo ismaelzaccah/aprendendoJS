bdacba8c1d31d2086acef2d6ab522ce38ff7b725
Ismael Zaccah <ismael.zaccah@gmail.com>
2021-07-20 17:58:45 -0300

    Iniciando com as Condicionais

---
+++ app2.js
@@ -0,0 +1,15 @@
+//--Condicinais IF ELSE--
+
+var isActive = false
+var message = ''
+
+if (isActive) { 
+    message = 'Está Ativo'
+} else {
+    message = 'Não está ativo'
+}
+
+console.log(message)
+
+//--Condicinais 
+
