8108152f3cedfc7454ac82d7e2bd0f068a9d51ad
Ismael Zaccah <ismael.zaccah@gmail.com>
2021-07-22 16:42:20 -0300

    Operadores Lógico e Terário

--- operadores.js
+++ operadores.js
@@ -1,8 +1,20 @@
+//--Operadores Lógicos
+
 var login = 'teste@teste.com'
 var pw = 'teste'
 
-if(login == 'teste@teste.com' \\ pw == 'teste'){
+if(login == 'teste@teste.com' && pw == 'teste'){
     console.log('liberado')
 } else{
     console.log('negado')
 }
+
+//--Operador Ternário
+
+var login = 'teste@teste.com'
+var pw = 'teste'
+
+var result = pw == 'teste' ? 'Liberado' : 'Negado'
+
+console.log(result)
+
