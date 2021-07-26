117d99b73d039d8fbefcec8b82f24c4df2f7f885
Ismael Zaccah <ismael.zaccah@gmail.com>
2021-07-26 17:47:52 -0300

    Estruturas de repetição For e While

---
+++ repeticao.js
@@ -0,0 +1,16 @@
+//--Estruturas de Repetição FOR e WHILE
+
+//--FOR-Quando fim do loop for conhecido
+
+for(var count=1; count <=10; count++){
+    console.log(count)
+}
+
+//--WHILE-Quando fim do loop for descohecido
+
+var i = 1
+
+while(i<=10) {
+    console.log(i)
+    i++
+}
