5ec803c4567093134cd29a8092b37dc99e600ed7
Ismael Zaccah <ismael.zaccah@gmail.com>
2021-08-08 08:28:54 -0300

    Alterando Estilos com Js

---
+++ altera-style.js
@@ -0,0 +1,10 @@
+//Alterando Estilos
+
+const divCadastro = document.querySelector('div#idade')
+//armazena div no objeto divCadastro
+
+divCadastro.style.backgroundColor = 'red'
+//palavras compostas do CSS passam pra cameoCase
+divCadastro.style.height = '200px'
+divCadastro.style.width = '200px'
+
