4b35d0485a216da43643319b58ceeba3da59f7ce
Ismael Zaccah <ismael.zaccah@gmail.com>
2021-08-10 12:09:07 -0300

    Armazenamento Local --localStorage.key = value--

---
+++ local-storage.js
@@ -0,0 +1,8 @@
+//--Armazenamento Local-- mais atual que o Cookies
+
+window.localStorage.theme = 'dark'
+// cria a Key --theme-- com Value --dark--
+// --window-- pode ser suprimido
+
+localStorage.zoomdapagina = 150
+localStorage.theme = 'light'
