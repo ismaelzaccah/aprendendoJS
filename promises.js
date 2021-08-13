7d36d74433aff9cd626c4a6db896a8de63fbd763
Ismael Zaccah <ismael.zaccah@gmail.com>
2021-08-13 17:55:14 -0300

    'desito das promisses, vou seguir em frente'

--- promises.js
+++ promises.js
@@ -1,19 +1,21 @@
 // --Promises - JS assíncrono--
 
-
+//
 var voceComprouOsProdutos = new Promise(function(resolve, reject){
+//Variavel armazena --new Promisse--
     setTimeout(function(){
         var products = ['Camisa','Sapato','Calça']
-        reject(products)
+        reject(products) // retorna --products-- ao final de --3000ms-- 
     }, 3000)
 })
 
 voceComprouOsProdutos
-    .then(function(products){
+    .then(function(products){ // Será executado em Caso de --resolve--
         console.log(products)
     })
-    .catch(function(error){
+    .catch(function(error){ // Será executado em Caso de --reject--
         console.log(error)
     })  
     
 console.log(voceComprouOsProdutos)
+// Irá mostrar status da promessa --resolve, reject-- e retornar valor armazenado no mesmo
