10930d0a5c5d8619883592d65364a2a59b92fb4b
Ismael Zaccah <ismael.zaccah@gmail.com>
2021-09-24 07:20:45 -0300

    Adicionado menssagens de erro

--- cep/cep.js
+++ cep/cep.js
@@ -18,21 +18,31 @@ function run(event){
     axios //funciona como uma --promisse-- 
     .get(`https://viacep.com.br/ws/${zipCode}/json/`)
     .then(function (response){ 
-        console.log(response.data)
         
-        createLine(response.data.logradouro)
-        createLine(response.data.bairro)
-        createLine(response.data.localidade + "/" + response.data.uf)
+        if (response.data.erro){ // cria um erro caso o cep seja invalido e tenha resposta undefinied e pula pro --catch--
+            throw new Error ("CEP  inválido")
+        }
+        
+        console.log(response.data) 
+        
+        contentMain.innerHTML = "" // Limpa o conteudo de --contentMain-- antes de escrever os dados recebidos
+
+        createLine(response.data.logradouro) // cria linha com Rua
+        createLine(response.data.bairro) // cria linha com Bairro
+        createLine(response.data.localidade + "/" + response.data.uf) // cria linha com Cidade/UF
     })
     .catch(function (error){
         console.log(error)
+        
+        contentMain.innerHTML = "" / // Limpa o conteudo de --contentMain-- antes de escrever os dados recebidos
+        createLine("Ops, algo deu errado!") // escreve uma linha com msg de erro 
     })
 }
 
 function createLine(text){
-    var line = document.createElement("p")
-    var content = document.createTextNode(text)
+    var line = document.createElement("p") //cria uma tag --p-- e atribui ao --line--
+    var content = document.createTextNode(text) // cria o texto que será atribuido a --content--
 
-    line.appendChild(content)
-    contentMain.appendChild(line)
+    line.appendChild(content) // coloca o --content-- dentro da tag atribuida ao --line--
+    contentMain.appendChild(line) // coloca a tag atribuida ao --line-- dentro da tag atibuida ao --contentMain--
 }
